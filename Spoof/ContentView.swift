f17a0e29a5a93426c8e009d994c358d56e957b79
Matt Rains <16088588+mattrainsxyz@users.noreply.github.com>
2023-01-13 14:17:29 -0800

    Initial Commit

---
+++ Spoof/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Spoof
+//
+//  Created by Matt Rains on 1/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spoof/SpoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoofApp.swift
+//  Spoof
+//
+//  Created by Matt Rains on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SpoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
